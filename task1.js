1e3e0731651f18c419e947a977af3c8ef2b02c6a
Urmas Kukk <urmaskukk@Urmass-MacBook-Air.local>
2021-09-08 16:18:52 +0300

    first commit

---
+++ task1.js
@@ -0,0 +1,15 @@
+const test = 'Tere'
+
+console.log(test)
+
+let a = 5
+let b = 10
+var c = a + b
+
+console.log(c)
+
+a = '5'
+b = 10
+var d = a + b
+
+console.log(d)
---
+++ task2.js
@@ -0,0 +1,8 @@
+const name = 'Urmas'
+const d = '28'
+const m = 'mai '
+const y = '1965'
+const hobby = 'vibu, nooled'
+const about = { name: name, birthday: d + '.' + m + y, hobby }
+
+console.log (about)
