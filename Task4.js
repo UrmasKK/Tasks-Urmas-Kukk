d478663674e6ee3fe841d7c53342cd521aaf5709
UrmasKK <90323833+UrmasKK@users.noreply.github.com>
2021-10-10 22:13:54 +0300

    Add files via upload

---
+++ Task4.js
@@ -0,0 +1,9 @@
+const colors = ['"Punane', 'Roheline', 'Valge', 'Must']
+
+const first = colors.toString(' " ') + '.' + '"'
+const second = colors.join(',') + ',' + '"'
+const third = colors.join('+') + '+' + '"'
+
+console.log(first)
+console.log(second)
+console.log(third)
