ae6157b81772ed0a97d612799fb708a1217e171f
UrmasKK <90323833+UrmasKK@users.noreply.github.com>
2021-10-10 22:15:22 +0300

    Add files via upload

---
+++ task2.1.js
@@ -0,0 +1,13 @@
+const name = 'Urmas'
+let d = 28;
+let m = 05
+let y = 1965
+let newBiDay = new Date('May 28, 1965');
+const hobby = 'vibu, nooled'
+
+const about = {
+    name: name,
+    date: newBiDay,
+    hobby: hobby
+}
+console.log (about)
